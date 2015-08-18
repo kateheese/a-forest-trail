c54c11072bfc87b4ebda021e691e871c20e1ef9a
kateheese <kateheese@gmail.com>
2015-08-17 19:07:13 -0600

    story 6

--- app.js
+++ app.js
@@ -21,90 +21,123 @@
     };
   };
 
-//get 'walk the trail' button
-var walkBtn = document.getElementById('walk');
-
-//Events that occur when the 'walk the trail' button is clicked
-walkBtn.addEventListener('click', function() {
-  //disable the button
-  this.setAttribute('disabled', 'true');
-  //enable button after 3 seconds
-    setTimeout(function() {
-        walkBtn.removeAttribute('disabled');
-    }, 3000);
-  //increases number of steps by 1 with each click
-  userStatus.steps += 1;
-  //decrease engery by 10 with each step
-  var energyCount = Math.floor(Math.random()* 10 + 1);
-  userStatus.energy -= energyCount;
-  //Update the DOM with each click
-  updateUserStatus();
-  //display'you continue down the trail' to the left after each click
-  var walkingText = document.createElement('div');
-  walkingText.innerHTML = 'you continue down the trail';
-  document.getElementById('left').appendChild(walkingText);
-
   //get buttons div
-    var buttonsDiv = document.getElementById('buttons');
+  var buttonsDiv = document.getElementById('buttons');
 
-  //Prompt user to pick berries every 10 steps
-  if(userStatus.steps % 10 === 0) {    
-    //create 'pick berries' button
-    var berryBtn = document.createElement('button');
-    berryBtn.id = 'pick-berries'
-    berryBtn.innerHTML = 'pick berries';
-    //add 'pick berries' button to buttons div
-    buttonsDiv.appendChild(berryBtn); 
+  //get 'walk the trail' button
+  var walkBtn = document.getElementById('walk');
 
-    //Events that occur when the 'pick berries' button is clicked
-    berryBtn.addEventListener('click', function() {
-      //collect random number of berries between 1 and 10
-      var berryCount = Math.floor(Math.random()* 10 + 1);
-      userStatus.berries += berryCount;
-      //Update the DOM with each click
-      updateUserStatus();
-      //display 'you picked {X} berries' every time the 'pick berries' button is clicked
-      var berriesText = document.createElement('div');
-      berriesText.innerHTML = 'you picked ' + berryCount + ' berries';
-      document.getElementById('left').appendChild(berriesText);
-      //remove 'pick berries' button
-      document.getElementById("buttons").removeChild(berryBtn);
-    });
-  }
+  //Events that occur when the 'walk the trail' button is clicked
+  walkBtn.addEventListener('click', function() {
+    //disable the button
+    this.setAttribute('disabled', 'true');
+    //enable button after 3 seconds
+      setTimeout(function() {
+        walkBtn.removeAttribute('disabled');
+      }, 3000);
+    //increases number of steps by 1 with each click
+    userStatus.steps += 1;
+    //decrease engery by 10 with each step
+    var energyCount = Math.floor(Math.random()* 10 + 1);
+    userStatus.energy -= energyCount;
+    //Update the DOM with each click
+    updateUserStatus();
+    //display'you continue down the trail' to the left after each click
+    var walkingText = document.createElement('div');
+    walkingText.innerHTML = 'you continue down the trail';
+    document.getElementById('left').appendChild(walkingText);
 
-  //Prompt user to look for water every 20 steps
-  if(userStatus.steps % 20 === 0) {
-    //create 'look for water' button
-    var waterBtn = document.createElement('button');
-    waterBtn.id = 'water'
-    waterBtn.innerHTML = 'look for water';
-    //add 'look for water' button to buttons div
-    buttonsDiv.appendChild(waterBtn);
 
-    //Events that occur when the 'look for water' button is clicked
-    waterBtn.addEventListener('click', function() {
-      //50% chance of finding water
-      var waterCount = Math.floor(Math.random()* 2 + 1);
-      var waterText = document.createElement('div');
-      if (waterCount === 1) {
-        //display 'you found water'
-        waterText.innerHTML = 'you found water';
-        document.getElementById('left').appendChild(waterText);
-        //increase water by 1 when water is found
-        userStatus.water += 1;
+    //Prompt user to pick berries every 10 steps
+    if(userStatus.steps % 10 === 0) {    
+      //create 'pick berries' button
+      var berryBtn = document.createElement('button');
+      berryBtn.id = 'pick-berries';
+      berryBtn.innerHTML = 'pick berries';
+      //add 'pick berries' button to buttons div
+      buttonsDiv.appendChild(berryBtn); 
+
+      //Events that occur when the 'pick berries' button is clicked
+      berryBtn.addEventListener('click', function() {
+        //collect random number of berries between 1 and 10
+        var berryCount = Math.floor(Math.random()* 10 + 1);
+        userStatus.berries += berryCount;
         //Update the DOM with each click
         updateUserStatus();
-      } else {
-        //display 'you could not find water'
-        waterText.innerHTML = 'you could not find any water';
-        document.getElementById('left').appendChild(waterText);
-      }
-      //remove 'look for water' button
-      document.getElementById("buttons").removeChild(waterBtn);
-    });
-  }
+        //display 'you picked {X} berries' every time the 'pick berries' button is clicked
+        var berriesText = document.createElement('div');
+        berriesText.innerHTML = 'you picked ' + berryCount + ' berries';
+        document.getElementById('left').appendChild(berriesText);
+        //remove 'pick berries' button
+        document.getElementById("buttons").removeChild(berryBtn);
 
-});
+        (function eatBerries() {
+          var berryInt = setInterval(function() {
+            //create 'eat berries' button
+            var eatBerriesBtn = document.createElement('button');
+            eatBerriesBtn.id = 'eat-berries'
+            eatBerriesBtn.innerHTML = 'eat berries';
+            //add 'eat berries' button to buttons div
+            buttonsDiv.appendChild(eatBerriesBtn);
+
+            if(buttonsDiv.contains(document.getElementById('eat-berries'))) {
+              clearInterval(berryInt);
+            } 
+
+            eatBerriesBtn.addEventListener('click', function() {
+              //On click of button decreases the berries count by 1, and increases energy by 2
+              userStatus.berries -= 1;
+              userStatus.energy += 2;
+              //Update the DOM with each click
+              updateUserStatus();
+              //display 'you ate berries and gained energy' every time the 'eat berries' button is clicked
+              var eatBerriesText = document.createElement('div');
+              eatBerriesText.innerHTML = 'you ate berries and gained energy';
+              document.getElementById('left').appendChild(eatBerriesText);
+              //remove 'eat berries' button
+              document.getElementById("buttons").removeChild(eatBerriesBtn);
+              //If userStatus berries is greater than zero, call eat berries function
+              if (userStatus.berries > 0) {
+                eatBerries();
+              }
+            })
+          }, 10000);
+        })();
+      });
+    }
+
+    //Prompt user to look for water every 20 steps
+    if(userStatus.steps % 20 === 0) {
+      //create 'look for water' button
+      var waterBtn = document.createElement('button');
+      waterBtn.id = 'water'
+      waterBtn.innerHTML = 'look for water';
+      //add 'look for water' button to buttons div
+      buttonsDiv.appendChild(waterBtn);
+
+      //Events that occur when the 'look for water' button is clicked
+      waterBtn.addEventListener('click', function() {
+        //50% chance of finding water
+        var waterCount = Math.floor(Math.random()* 2 + 1);
+        var waterText = document.createElement('div');
+        if (waterCount === 1) {
+          //display 'you found water'
+          waterText.innerHTML = 'you found water';
+          document.getElementById('left').appendChild(waterText);
+          //increase water by 1 when water is found
+          userStatus.water += 1;
+          //Update the DOM with each click
+          updateUserStatus();
+        } else {
+          //display 'you could not find water'
+          waterText.innerHTML = 'you could not find any water';
+          document.getElementById('left').appendChild(waterText);
+        }
+        //remove 'look for water' button
+        document.getElementById("buttons").removeChild(waterBtn);
+      });
+    }
+  });
 
 //Update DOM on page load
 updateUserStatus();
